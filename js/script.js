8f048be21fc2cf80523b6d26ad3aff456d50e11e
omarwebdeve <omarf977865@gmail.com>
2022-07-27 16:08:48 +0600

    mobile design completed

---
+++ js/script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                penguin: '#fffbf0'
+            },
+            fontFamily:{
+                bebas:['Bebas Neue','cursive'],
+                mont:['Montserrat','sans-serif']
+            }
+        }
+    }
+}
+    // font-family: 'Bebas Neue', cursive;
+    // font-family: 'Montserrat', sans-serif; 
